80777c5720396e5d2fed64cfa30a7b584e184021
Harakirikungen <haikara98@gmail.com>
2025-09-07 19:37:26 +0200

    delete

--- src/components/ui/Button.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function Button({ children, className, ...props }) {
-  return <button className={`font-semibold rounded-xl px-4 py-2 transition ${className}`} {...props}>{children}</button>;
-}
